54eb0ba0798a43c5c11fb74f63481521f965ea81
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2023-02-24 17:41:46 +0200

    Remove unused code

--- src/Commands/ShowModelCommand.php
+++ src/Commands/ShowModelCommand.php
@@ -3,7 +3,6 @@
 namespace FumeApp\ModelTyper\Commands;
 
 use Illuminate\Foundation\Console\ShowModelCommand as BaseCommand;
-use Illuminate\Support\Composer;
 
 /**
  * A wrapper command for Laravel default model:show to add customizaton for model generation.
@@ -17,19 +16,6 @@ class ShowModelCommand extends BaseCommand
                 {--custom-relationships= : Custom relationships that should be included, separated by commas}
                 {--database= : The database connection to use}
                 {--json : Output the model as JSON}';
-
-    // /**
-    //  * Create a new command instance.
-    //  *
-    //  * @return void
-    //  */
-    // public function __construct(Composer $composer = null)
-    // {
-    //     parent::__construct($composer);
-
-    //     $customMethods = collect(explode(',', $this->option('custom-relationships')))->map(fn($method) => trim($method));
-    //     $this->relationMethods = array_merge($this->relationMethods, $customMethods);
-    // }
 
     public function handle()
     {
