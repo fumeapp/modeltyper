cdb4de83081a7eb36d4e998c8d12eb5d8150c27f
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2023-03-28 22:38:17 +0300

    Add example output expectation test

--- test/Tests/Feature/Console/ModelTyperCommandTest.php
+++ test/Tests/Feature/Console/ModelTyperCommandTest.php
@@ -2,27 +2,38 @@
 
 namespace Tests\Feature\Console;
 
+use App\Models\User;
 use FumeApp\ModelTyper\Commands\ModelTyperCommand;
 use Tests\Feature\TestCase;
 use Tests\Traits\GeneratesOutput;
+use Tests\Traits\UsesInputFiles;
 
 class ModelTyperCommandTest extends TestCase
 {
-    use GeneratesOutput;
+    use GeneratesOutput, UsesInputFiles;
 
     protected function tearDown() : void
     {
         parent::tearDown();
+
+        // NOTE Not really necessary at the moment, but might be useful in the future
+        // if something like --outputfile option is added to the command
         $this->deleteOutput();
     }
 
-    public function testBaseCommandCanBeExecutedSuccessfully()
+    public function testCommandCanBeExecutedSuccessfully()
     {
         $this->artisan(ModelTyperCommand::class)->assertSuccessful();
     }
 
-    public function testBaseCommandFailsWhenTryingToResolveAbstractModelThatHasNoBinding()
+    public function testCommandFailsWhenTryingToResolveAbstractModelThatHasNoBinding()
     {
         $this->artisan(ModelTyperCommand::class, ['--resolve-abstract' => true])->assertFailed();
+    }
+
+    public function testCommandGeneratesExpectedOutputForUserModel()
+    {
+        $expected = $this->getExpectedContent('example.ts');
+        $this->artisan(ModelTyperCommand::class, ['--model' => User::class])->expectsOutput($expected);
     }
 }
