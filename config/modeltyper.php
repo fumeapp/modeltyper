073f2e703381200f5ed4f376c52dd18a9164991e
AyrtonAndino <ayrton.andino@outlook.com>
2024-05-02 13:41:35 -0300

    Update modeltyper.php
    add new custom_mappings to config, fix typo

--- config/modeltyper.php
+++ config/modeltyper.php
@@ -1,9 +1,26 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Override default mappings or add new ones
+    |--------------------------------------------------------------------------
+    |
+    | Custom mappings allow you to add support for types that are considered
+    | unknown or override existing mappings.
+    |
+    | For example:
+    | 'binary' => 'Blob',
+    | 'point' => 'Point',
+    | 'year' => 'string',
+    */
+    'custom_mappings' => [
+        // 'binary' => 'Blob',
+    ],
+
     /**
      * Custom relationships allows you to add support for relationships from
-     * extenal packages that are not a part of the Laravel core. Note that
+     * external packages that are not a part of the Laravel core. Note that
      * relationship method names are case sensitive.
      */
     'custom_relationships' => [
