3644f21d8c9ef754c03071d30925e058701be656
Ayrton Andino Bonazzola <60512432+ayrtonandino@users.noreply.github.com>
2024-11-28 19:00:47 -0300

    add param to RunModelInspector

--- src/Actions/RunModelInspector.php
+++ src/Actions/RunModelInspector.php
@@ -17,6 +17,7 @@ class RunModelInspector
      *
      * @see https://github.com/laravel/framework/blob/11.x/src/Illuminate\Database\Eloquent\ModelInspector.php
      *
+     * @param  class-string<\Illuminate\Database\Eloquent\Model>  $model
      * @return array{"class": class-string<\Illuminate\Database\Eloquent\Model>, database: string, table: string, policy: class-string|null, attributes: \Illuminate\Support\Collection, relations: \Illuminate\Support\Collection, events: \Illuminate\Support\Collection, observers: \Illuminate\Support\Collection, collection: class-string<\Illuminate\Database\Eloquent\Collection<\Illuminate\Database\Eloquent\Model>>, builder: class-string<\Illuminate\Database\Eloquent\Builder<\Illuminate\Database\Eloquent\Model>>}|null
      */
     public function __invoke(string $model, bool $resolveAbstract = false): ?array
