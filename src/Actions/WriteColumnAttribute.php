8c389fa1a109f834bd9df2f823c6ed34308351e0
tanner <tcamp022@gmail.com>
2026-06-11 23:15:12 -0400

    Add 'optional' attribute to parameter type definition in WriteColumnAttribute

--- src/Actions/WriteColumnAttribute.php
+++ src/Actions/WriteColumnAttribute.php
@@ -21,7 +21,7 @@ class WriteColumnAttribute
      * Get model columns and attributes to the output.
      *
      * @param  ReflectionClass<Model>  $reflectionModel
-     * @param  array{name: string, type?: string|null, increments: bool, nullable: bool, default: mixed, unique: bool, fillable: bool, hidden?: bool, appended: mixed, cast?: string|null, forceType?: bool}  $attribute
+     * @param  array{name: string, type?: string|null, increments: bool, nullable: bool, default: mixed, unique: bool, fillable: bool, hidden?: bool, appended: mixed, cast?: string|null, forceType?: bool, optional?: bool}  $attribute
      * @param  array<string, string>  $mappings
      * @return array{array{name: string, type: string}, ReflectionClass|null}|array{string, ReflectionClass|null}|array{null, null}
      */
